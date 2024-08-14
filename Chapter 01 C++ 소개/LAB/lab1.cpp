5d2d9e5f256dbc34dd16def2bbb6e7487a6cbb9e
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-08-14 23:21:42 +0900

    Create lab1.cpp

---
+++ Chapter 01 C++ 소개/LAB/lab1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y, z;
+    cout << "첫 번째 수를 입력하시오: ";
+    cin >> x;
+    cout << "두 번째 수를 입력하시오: ";
+    cin >> y;
+    z = x + y;
+    cout << "두 수의 합은 " << z << "입니다."<< endl;
+    return 0;
+}
