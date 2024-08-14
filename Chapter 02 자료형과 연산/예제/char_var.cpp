26054f59a99cdb1eaa9c8883d533ca34a310db87
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-08-15 00:09:35 +0900

    Create char_var.cpp

---
+++ Chapter 02 자료형과 연산/예제/char_var.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; // 이름 공간 설정
+
+int main()
+{
+    char c; // 변수 선언
+    
+    cout << "문자를 입력하시오: "; // 입력 안내문
+    cin >> c;
+    
+    cout << "입력된 문자는 " << c << "입니다." << endl;
+    
+    return 0;
+}
