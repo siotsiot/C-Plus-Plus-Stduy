3ff67a9c285d78ea432b12a3bb1d3019d4918c8f
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-08-15 07:06:50 +0900

    Create nested_loop.cpp

---
+++ Chapter 03 선택과 반복/예제/nested_loop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int y = 0; y < 5; y++)
+    {
+        for (int x = 0; x < 10; x++)
+            cout << "*";
+        cout << endl;
+    }
+
+    return 0;
+}
