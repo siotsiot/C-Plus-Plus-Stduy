45410f91316d858032f81864c7b56be0d8f3026e
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-08-15 02:51:32 +0900

    Create arithmetic.cpp

---
+++ Chapter 02 자료형과 연산/예제/arithmetic.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << 3.0 + 2.0 << endl; // 실수 + 실수
+	cout << 3.0 - 2.0 << endl; // 실수 - 실수
+	cout << 3.0 * 2.0 << endl; // 실수 * 실수
+	cout << 3.0 / 2 << endl; // 실수 / 정수
+	cout << 3 % 2 << endl; // 정수 / 정수
+
+	return 0;
+}
