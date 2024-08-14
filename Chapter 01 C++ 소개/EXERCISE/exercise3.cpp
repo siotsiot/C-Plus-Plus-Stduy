e33c006ab3f3f1e128a79e39bab88a9dd08b6703
Seung-su Lee <60792515+siotsiot@users.noreply.github.com>
2024-08-15 03:06:26 +0900

    Delete Chapter 01 C++ 소개/EXERCISE directory

--- Chapter 01 C++ 소개/EXERCISE/exercise3.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream> // 입출력에 필요한 클래스와 객체 정의
-using namespace std; // 현재 사용하는 이름 공간을 std로 설정
-
-int main() // 함수의 헤더
-{
-    cout << "Hello World!\n"; //화면에 해당 문자열 출력
-    cout << "C++ is fast\n"; // 상동
-    return 0;
-}
